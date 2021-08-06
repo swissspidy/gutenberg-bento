832b635ad7eac1f4ffd3b43179a4fa370ad1a7bf
Pascal Birchler <pascalb@google.com>
2021-08-06 16:26:41 +0200

    PHPUnit fix

--- tests/phpunit/tests/plugin.php
+++ tests/phpunit/tests/plugin.php
@@ -2,6 +2,8 @@
 
 namespace Google\Gutenberg_Bento\Tests;
 
+use WP_UnitTestCase;
+
 class Plugin_Test extends WP_UnitTestCase {
 	public function test_boostrap() {
 		$this->assertSame( 10, has_action( 'init', '\Google\Gutenberg_Bento\register_bento_assets' ) );
