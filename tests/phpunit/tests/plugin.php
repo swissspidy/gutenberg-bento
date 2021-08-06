2c053bbedbcfab4c7c3aca8d9d16d5fa81699757
Pascal Birchler <pascalb@google.com>
2021-08-06 16:13:49 +0200

    Add some CI workflows

---
+++ tests/phpunit/tests/plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Google\Gutenberg_Bento\Tests;
+
+class Plugin_Test extends WP_UnitTestCase {
+	public function test_boostrap() {
+		$this->assertSame( 10, has_action( 'init', '\Google\Gutenberg_Bento\register_bento_assets' ) );
+	}
+}
