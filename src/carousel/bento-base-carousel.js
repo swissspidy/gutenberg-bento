524c3fe0d4bf611fe4a33aaeaa090e1a9204bdbd
Pascal Birchler <pascalb@google.com>
2022-02-21 21:35:44 +0100

    Lint fix

--- src/carousel/bento-base-carousel.js
+++ src/carousel/bento-base-carousel.js
@@ -2,7 +2,7 @@ import { defineElement } from '@bentoproject/base-carousel';
 
 // Just so webpack generates a separate stylesheet for us to enqueue in WP.
 import '@bentoproject/base-carousel/styles.css';
-import '@bentoproject/base-carousel/web-component.css';
+import '@bentoproject/base-carousel/web-component.css'; // eslint-disable-line import/no-unresolved -- False positive.
 
 if (
 	document.readyState === 'complete' ||
