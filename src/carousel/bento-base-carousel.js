af0ba9598130a4752a83d6b2af5050142f5ee2ec
Pascal Birchler <pascalb@google.com>
2022-02-21 21:28:53 +0100

    Update imports

--- src/carousel/bento-base-carousel.js
+++ src/carousel/bento-base-carousel.js
@@ -1,7 +1,8 @@
-import { defineElement } from '@bentoproject/base-carousel/web-component';
+import { defineElement } from '@bentoproject/base-carousel';
 
 // Just so webpack generates a separate stylesheet for us to enqueue in WP.
 import '@bentoproject/base-carousel/styles.css';
+import '@bentoproject/base-carousel/web-component.css';
 
 if (
 	document.readyState === 'complete' ||
