4b02c69b87b7397f55581e4b6041c9d2d19d6188
Adam Zieliński <adam@adamziel.com>
2022-03-20 18:56:21 +0100

    BentoCarousel block

--- src/carousel/save.js
+++ src/carousel/save.js
@@ -1,63 +1,35 @@
 /**
  * WordPress dependencies
  */
-import { RichText, useBlockProps } from '@wordpress/block-editor';
-import { __ } from '@wordpress/i18n';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function save({ attributes }) {
-	const { images, autoAdvance, loop, snap } = attributes;
-
-	const className = 'gutenberg-bento-carousel-wrapper';
+	const {
+		autoAdvance,
+		loop,
+		snap,
+		direction,
+		mixedLengths,
+		controls,
+		orientation,
+		visibleCount,
+	} = attributes;
 
 	return (
 		<figure {...useBlockProps.save()}>
 			<bento-base-carousel
-				className={className}
+				className={'gutenberg-bento-carousel-wrapper'}
 				auto-advance={autoAdvance ? 'true' : undefined}
 				loop={loop ? 'true' : undefined}
 				snap={snap ? 'true' : undefined}
+				dir={direction ? 'true' : undefined}
+				orientation={orientation}
+				controls={controls}
+				visible-count={visibleCount + ''}
+				mixed-lengths={mixedLengths ? 'true' : undefined}
 			>
-				{images.map((image) => {
-					let href;
-
-					const img = (
-						<img
-							src={image.url}
-							alt={image.alt}
-							data-id={image.id}
-							data-full-url={image.fullUrl}
-							data-link={image.link}
-							className={image.id ? `wp-image-${image.id}` : null}
-						/>
-					);
-
-					return (
-						<div
-							key={image.id || image.url}
-							className="gutenberg-bento-carousel-item"
-						>
-							<figure>
-								{href ? <a href={href}>{img}</a> : img}
-								{!RichText.isEmpty(image.caption) && (
-									<RichText.Content
-										tagName="figcaption"
-										className="gutenberg-bento-carousel-item__caption"
-										value={image.caption}
-									/>
-								)}
-							</figure>
-						</div>
-					);
-				})}
+				<InnerBlocks.Content />
 			</bento-base-carousel>
-			<div className="gutenberg-bento-carousel-buttons">
-				<button className="gutenberg-bento-carousel-buttons__prev">
-					{__('Previous', 'gutenberg-bento')}
-				</button>
-				<button className="gutenberg-bento-carousel-buttons__next">
-					{__('Next', 'gutenberg-bento')}
-				</button>
-			</div>
 		</figure>
 	);
 }
