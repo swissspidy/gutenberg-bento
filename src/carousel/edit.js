3adaa54ab1b7fc29bd683faeafdca01708862bd9
Adam Zieliński <adam@adamziel.com>
2022-03-20 19:18:37 +0100

    Prettier

--- src/carousel/edit.js
+++ src/carousel/edit.js
@@ -41,7 +41,7 @@ import './editor.css';
 
 const ALLOWED_MEDIA_TYPES = ['image'];
 
-function CarouselEdit(props) {
+function CarouselEdit( props ) {
 	const { attributes, setAttributes, clientId, noticeOperations } = props;
 	const {
 		autoAdvance,
@@ -61,154 +61,154 @@ function CarouselEdit(props) {
 			orientation,
 			__experimentalBlocksListItemsWrapper: (
 				<BentoBaseCarousel
-					ref={carouselRef}
-					dir={direction}
-					orientation={orientation}
-					controls={controls}
-					autoAdvance={autoAdvance}
-					visibleCount={visibleCount}
-					mixedLengths={mixedLengths}
-					loop={loop}
-					snap={snap}
+					ref={ carouselRef }
+					dir={ direction }
+					orientation={ orientation }
+					controls={ controls }
+					autoAdvance={ autoAdvance }
+					visibleCount={ visibleCount }
+					mixedLengths={ mixedLengths }
+					loop={ loop }
+					snap={ snap }
 				/>
 			),
-		}
+		},
 	);
 	const innerBlocks = useSelect(
-		(select) => {
-			return select(blockEditorStore).getBlock(clientId)?.innerBlocks;
+		( select ) => {
+			return select( blockEditorStore ).getBlock( clientId )?.innerBlocks;
 		},
-		[clientId]
+		[clientId],
 	);
 
 	const hasImages = innerBlocks.length > 0;
 	const imagesIds = hasImages
-		? innerBlocks.map((image) => image?.attributes?.id)
+		? innerBlocks.map( ( image ) => image?.attributes?.id )
 		: [];
-	const hasImageIds = imagesIds.some((id) => id);
+	const hasImageIds = imagesIds.some( ( id ) => id );
 
-	const { replaceInnerBlocks, selectBlock } = useDispatch(blockEditorStore);
+	const { replaceInnerBlocks, selectBlock } = useDispatch( blockEditorStore );
 
-	function updateImages(selectedImages) {
+	function updateImages( selectedImages ) {
 		const newFileUploads =
-			Object.prototype.toString.call(selectedImages) ===
+			Object.prototype.toString.call( selectedImages ) ===
 			'[object FileList]';
 
 		const imageArray = newFileUploads
-			? Array.from(selectedImages).map((file) => {
-					console.log({ file });
-					if (!file.url) {
-						return pickRelevantMediaFiles({
-							url: createBlobURL(file),
-						});
-					}
+			? Array.from( selectedImages ).map( ( file ) => {
+				console.log( { file } );
+				if ( !file.url ) {
+					return pickRelevantMediaFiles( {
+						url: createBlobURL( file ),
+					} );
+				}
 
-					return file;
-			  })
+				return file;
+			} )
 			: selectedImages;
 
-		if (!imageArray.every(isValidFileType)) {
+		if ( !imageArray.every( isValidFileType ) ) {
 			noticeOperations.removeAllNotices();
 			noticeOperations.createErrorNotice(
 				__(
-					'If uploading to a gallery all files need to be image formats'
+					'If uploading to a gallery all files need to be image formats',
 				),
-				{ id: 'gallery-upload-invalid-file' }
+				{ id: 'gallery-upload-invalid-file' },
 			);
 		}
 
 		const processedImages = imageArray
-			.filter((file) => file.url || isValidFileType(file))
-			.map((file) => {
-				if (!file.url) {
-					return pickRelevantMediaFiles({
-						url: createBlobURL(file),
-					});
+			.filter( ( file ) => file.url || isValidFileType( file ) )
+			.map( ( file ) => {
+				if ( !file.url ) {
+					return pickRelevantMediaFiles( {
+						url: createBlobURL( file ),
+					} );
 				}
 
 				return file;
-			});
+			} );
 
 		// Because we are reusing existing innerImage blocks any reordering
 		// done in the media library will be lost so we need to reapply that ordering
 		// once the new image blocks are merged in with existing.
 		const newOrderMap = processedImages.reduce(
-			(result, image, index) => ((result[image.id] = index), result),
-			{}
+			( result, image, index ) => ( ( result[ image.id ] = index ), result ),
+			{},
 		);
 
 		const existingImageBlocks = !newFileUploads
-			? innerBlocks.filter((block) =>
-					processedImages.find(
-						(img) => img.id === block.attributes.id
-					)
-			  )
+			? innerBlocks.filter( ( block ) =>
+				processedImages.find(
+					( img ) => img.id === block.attributes.id,
+				),
+			)
 			: innerBlocks;
 
 		const newImageList = processedImages.filter(
-			(img) =>
+			( img ) =>
 				!existingImageBlocks.find(
-					(existingImg) => img.id === existingImg.attributes.id
-				)
+					( existingImg ) => img.id === existingImg.attributes.id,
+				),
 		);
 
-		const newBlocks = newImageList.map((image) => {
-			return createBlock('core/image', {
+		const newBlocks = newImageList.map( ( image ) => {
+			return createBlock( 'core/image', {
 				id: image.id,
 				url: image.url,
 				caption: image.caption,
 				alt: image.alt,
-			});
-		});
+			} );
+		} );
 
-		if (newBlocks?.length > 0) {
-			selectBlock(newBlocks[0].clientId);
+		if ( newBlocks?.length > 0 ) {
+			selectBlock( newBlocks[ 0 ].clientId );
 		}
 
 		replaceInnerBlocks(
 			clientId,
-			concat(existingImageBlocks, newBlocks).sort(
-				(a, b) =>
-					newOrderMap[a.attributes.id] - newOrderMap[b.attributes.id]
-			)
+			concat( existingImageBlocks, newBlocks ).sort(
+				( a, b ) =>
+					newOrderMap[ a.attributes.id ] - newOrderMap[ b.attributes.id ],
+			),
 		);
 	}
 
 	function initTheBlock() {
-		replaceInnerBlocks(clientId, [createBlock('core/cover', {})]);
+		replaceInnerBlocks( clientId, [createBlock( 'core/cover', {} )] );
 	}
 
 	const goToNextSlide = () => {
-		if (carouselRef.current) {
+		if ( carouselRef.current ) {
 			carouselRef.current.next();
 		}
 	};
 
 	const goToPreviousSlide = () => {
-		if (carouselRef.current) {
+		if ( carouselRef.current ) {
 			carouselRef.current.prev();
 		}
 	};
 
 	const blockProps = useBlockProps();
 
-	if (!innerBlocks.length) {
+	if ( !innerBlocks.length ) {
 		return (
 			<div className="components-placeholder block-editor-media-placeholder is-large">
 				<div className="components-placeholder__label">
-					{__('Bento Carousel', 'gutenberg-bento')}
+					{ __( 'Bento Carousel', 'gutenberg-bento' ) }
 				</div>
 				<fieldset className="components-placeholder__fieldset">
 					<legend className="components-placeholder__instructions">
-						{__(
+						{ __(
 							'Build your super cool carousel.',
-							'gutenberg-bento'
-						)}
+							'gutenberg-bento',
+						) }
 					</legend>
 					<div className="components-form-file-upload">
 						<button
 							type="button"
-							onClick={initTheBlock}
+							onClick={ initTheBlock }
 							className="components-button block-editor-media-placeholder__button block-editor-media-placeholder__upload-button is-primary"
 						>
 							Start with a cover block
@@ -222,146 +222,146 @@ function CarouselEdit(props) {
 	return (
 		<>
 			<InspectorControls>
-				<PanelBody title={__('Carousel Settings', 'gutenberg-bento')}>
+				<PanelBody title={ __( 'Carousel Settings', 'gutenberg-bento' ) }>
 					<ToggleControl
-						label={__('Auto Advance', 'gutenberg-bento')}
-						checked={!!autoAdvance}
-						onChange={() =>
-							setAttributes({ autoAdvance: !autoAdvance })
+						label={ __( 'Auto Advance', 'gutenberg-bento' ) }
+						checked={ !!autoAdvance }
+						onChange={ () =>
+							setAttributes( { autoAdvance: !autoAdvance } )
 						}
 					/>
 					<ToggleControl
-						label={__('Loop', 'gutenberg-bento')}
-						checked={!!loop}
-						onChange={() => setAttributes({ loop: !loop })}
+						label={ __( 'Loop', 'gutenberg-bento' ) }
+						checked={ !!loop }
+						onChange={ () => setAttributes( { loop: !loop } ) }
 					/>
 					<ToggleControl
-						label={__('Snap', 'gutenberg-bento')}
-						checked={!!snap}
-						onChange={() => setAttributes({ snap: !snap })}
+						label={ __( 'Snap', 'gutenberg-bento' ) }
+						checked={ !!snap }
+						onChange={ () => setAttributes( { snap: !snap } ) }
 					/>
 					<ToggleControl
-						label={__('Mixed lengths', 'gutenberg-bento')}
-						checked={!!mixedLengths}
-						onChange={() =>
-							setAttributes({ mixedLengths: !mixedLengths })
+						label={ __( 'Mixed lengths', 'gutenberg-bento' ) }
+						checked={ !!mixedLengths }
+						onChange={ () =>
+							setAttributes( { mixedLengths: !mixedLengths } )
 						}
 					/>
 					<SelectControl
-						label={__('Direction', 'gutenberg-bento')}
-						value={direction}
+						label={ __( 'Direction', 'gutenberg-bento' ) }
+						value={ direction }
 						// `undefined` is required for the preload attribute to be unset.
-						onChange={(value) => {
-							console.log({
+						onChange={ ( value ) => {
+							console.log( {
 								direction: value,
-							});
-							setAttributes({
+							} );
+							setAttributes( {
 								direction: value,
-							});
-						}}
-						options={[
+							} );
+						} }
+						options={ [
 							{
 								value: 'ltr',
-								label: __('Left to right', 'gutenberg-bento'),
+								label: __( 'Left to right', 'gutenberg-bento' ),
 							},
 							{
 								value: 'rtl',
-								label: __('Right to left', 'gutenberg-bento'),
+								label: __( 'Right to left', 'gutenberg-bento' ),
 							},
-						]}
+						] }
 					/>
 					<SelectControl
-						label={__('Orientation', 'gutenberg-bento')}
-						value={orientation}
+						label={ __( 'Orientation', 'gutenberg-bento' ) }
+						value={ orientation }
 						// `undefined` is required for the preload attribute to be unset.
-						onChange={(value) =>
-							setAttributes({
+						onChange={ ( value ) =>
+							setAttributes( {
 								orientation: value,
-							})
+							} )
 						}
-						options={[
+						options={ [
 							{
 								value: 'horizontal',
-								label: __('Horizontal', 'gutenberg-bento'),
+								label: __( 'Horizontal', 'gutenberg-bento' ),
 							},
 							{
 								value: 'vertical',
-								label: __('Vertical', 'gutenberg-bento'),
+								label: __( 'Vertical', 'gutenberg-bento' ),
 							},
-						]}
+						] }
 					/>
 					<SelectControl
-						label={__('Controls', 'gutenberg-bento')}
-						value={controls}
+						label={ __( 'Controls', 'gutenberg-bento' ) }
+						value={ controls }
 						// `undefined` is required for the preload attribute to be unset.
-						onChange={(value) =>
-							setAttributes({
+						onChange={ ( value ) =>
+							setAttributes( {
 								controls: value,
-							})
+							} )
 						}
-						options={[
+						options={ [
 							{
 								value: 'always',
-								label: __('Always', 'gutenberg-bento'),
+								label: __( 'Always', 'gutenberg-bento' ),
 							},
 							{
 								value: 'auto',
-								label: __('Auto', 'gutenberg-bento'),
+								label: __( 'Auto', 'gutenberg-bento' ),
 							},
 							{
 								value: 'never',
-								label: __('Never', 'gutenberg-bento'),
+								label: __( 'Never', 'gutenberg-bento' ),
 							},
-						]}
+						] }
 					/>
 					<TextControl
-						label={__('Visible slides', 'gutenberg-bento')}
+						label={ __( 'Visible slides', 'gutenberg-bento' ) }
 						type="number"
-						onChange={(value) => {
-							const int = parseInt(value, 10);
+						onChange={ ( value ) => {
+							const int = parseInt( value, 10 );
 
-							setAttributes({
+							setAttributes( {
 								// It should be possible to unset the value,
 								// e.g. with an empty string.
-								visibleCount: isNaN(int) ? 1 : int,
-							});
-						}}
-						value={visibleCount}
+								visibleCount: isNaN( int ) ? 1 : int,
+							} );
+						} }
+						value={ visibleCount }
 						step="1"
 					/>
 				</PanelBody>
 			</InspectorControls>
 			<BlockControls group="other">
 				<MediaReplaceFlow
-					allowedTypes={ALLOWED_MEDIA_TYPES}
+					allowedTypes={ ALLOWED_MEDIA_TYPES }
 					accept="image/*"
-					handleUpload={false}
-					onSelect={updateImages}
-					name={__('Add images')}
-					multiple={true}
-					mediaIds={imagesIds}
-					addToGallery={hasImageIds}
+					handleUpload={ false }
+					onSelect={ updateImages }
+					name={ __( 'Add images' ) }
+					multiple={ true }
+					mediaIds={ imagesIds }
+					addToGallery={ hasImageIds }
 				/>
 				<ToolbarGroup>
 					<ToolbarButton
-						title={__('Previous slide')}
-						icon={arrowLeft}
-						onClick={goToPreviousSlide}
+						title={ __( 'Previous slide' ) }
+						icon={ arrowLeft }
+						onClick={ goToPreviousSlide }
 					/>
 					<ToolbarButton
-						title={__('Next slide')}
-						icon={arrowRight}
-						onClick={goToNextSlide}
+						title={ __( 'Next slide' ) }
+						icon={ arrowRight }
+						onClick={ goToNextSlide }
 					/>
 				</ToolbarGroup>
 			</BlockControls>
-			<figure {...blockProps}>
+			<figure { ...blockProps }>
 				<div className="gutenberg-bento-carousel-wrapper">
-					<div {...innerBlocksProps} />
+					<div { ...innerBlocksProps } />
 				</div>
 			</figure>
 		</>
 	);
 }
 
-export default compose([withNotices])(CarouselEdit);
+export default compose( [withNotices] )( CarouselEdit );
